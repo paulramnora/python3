8cf717c1549b81c56eb5cd40f97c21cfeb46e925
Paul Ramnora <paulramnora@gmail.com>
2026-02-01 08:06:56 +0000

    Create format-full-name.py
    Initial commit. Create file: format-full-name.py
    Asks user to enter their: first/middle/last name...;
    then, prints out the name specially formatted.

---
+++ language/input/text/format-full-name.py
@@ -0,0 +1,19 @@
+# Program: Get user first/last/middle name...
+#          then, print out name specially formatted:
+#          Full name: fName mName lName
+#          First and last name; fName lName
+#          Initials: lName. mName. LName.
+
+# input...
+
+fName=input("Enter first name: ")
+mName=input("Enter middle name: ")
+lName=input("Enter last name: ")
+
+# output...
+
+print() # prints a blank vertical line...for clearer output spacking.
+
+print(f"Your full name is: {fName} {mName} {lName}")
+print(f"Your first and last name are: {fName} {lName}")
+print(f"Your initials are: {fName[0]}. {mName[0]}. {lName[0]}.") 
