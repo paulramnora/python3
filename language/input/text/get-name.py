23c49867e577f3a9e57ab04fcb79abc82d1c6944
paulramnora <paulramnora@gmail.com>
2026-02-01 07:42:08 +0000

    Create get-name.py
    Initial commit. Created file: [get-name.py]
    Asks user to enter their name...; followed by a welcome message which greets them by using their name.

---
+++ language/input/text/get-name.py
@@ -0,0 +1,7 @@
+# Program: Get-name...
+# 1. Asks used to enter their name by typing it in at the keyboard; then, pressing the [ENTER] key to complete their data entry. 
+# 2. Followed by printing on screen a welcome message saying: 'Welcome, name!'                              
+
+print("Enter name: ")
+name=input()
+print(f"Welcome, {name}!")
